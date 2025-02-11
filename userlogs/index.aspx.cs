f2824218e9b33a40cb83ef7a2db197720e483cc5
Jessa <jessamsaquin.dev@gmail.com>
2025-02-11 21:10:29 +0800

    clean few lines and bugs when executing the file

--- userlogs/index.aspx.cs
+++ userlogs/index.aspx.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Data.SqlClient;
 using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
 
 namespace userlogs
 {
@@ -47,56 +43,60 @@ namespace userlogs
             if (txtuserName.Text == "" || txtPassword.Text == "")
             {
                 ErrorMgs.Text = "Please fill all the fields!";
-                
-            }else if (txtPassword.Text != txtconfirmPassword.Text)
+
+            }
+            else if (txtPassword.Text != txtconfirmPassword.Text)
             {
                 ErrorMgs.Text = "Password does not match!";
             }
             else
             {
-                
-           
-            using (SqlConnection conn = new SqlConnection(connStr))
-            {
-               
-                SqlCommand cmd = new SqlCommand("UserAddEditDel", conn);
+
+
+                using (SqlConnection conn = new SqlConnection(connStr))
                 {
-                    cmd.CommandType = CommandType.StoredProcedure;
 
-                    // Add stored procedure parameters
-                    cmd.Parameters.AddWithValue("@UserId", string.IsNullOrEmpty(hfUserID.Value) ? Guid.NewGuid() : Guid.Parse(hfUserID.Value));
-                    cmd.Parameters.AddWithValue("@Username", txtuserName.Text.Trim());
-                    cmd.Parameters.AddWithValue("@PasswordHash", txtPassword.Text.Trim());
-                    cmd.Parameters.AddWithValue("@LastName", txtlastName.Text.Trim());
-                    cmd.Parameters.AddWithValue("@FirstName", txtfirstName.Text.Trim());
-                    cmd.Parameters.AddWithValue("@MiddleName", txtmidName.Text.Trim());
-                    cmd.Parameters.AddWithValue("@NickName", txtnickName.Text.Trim());
-                    cmd.Parameters.AddWithValue("@EmailAddress", txtemailAdd.Text.Trim());
-                    cmd.Parameters.AddWithValue("@Department", dlistDepartment.SelectedValue);
-                    cmd.Parameters.AddWithValue("@TimeZone", Convert.ToInt32(dlistTimeZone.SelectedValue));
-                    cmd.Parameters.AddWithValue("@UserType", Convert.ToByte(RadiouserType.SelectedValue));
-                    cmd.Parameters.AddWithValue("@UserStatus", Convert.ToByte(dlistUserstat.SelectedValue));
+                    SqlCommand cmd = new SqlCommand("UserAddEditDel", conn);
+                    {
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        conn.Open();
 
-                    //cmd.Parameters.AddWithValue("@ArchivedDate", archivedDate);
-                    //cmd.Parameters.AddWithValue("@IsIn", isIn);
-                    conn.Open();
-                    clear();
-                    // Execute the stored procedure
-                    cmd.ExecuteNonQuery();
-                    SuccessMgs.Text = "User Added Successfully!";
-                }
+                        // Add stored procedure parameters
+                        cmd.Parameters.AddWithValue("@UserId", string.IsNullOrEmpty(hfUserID.Value) ? Guid.NewGuid() : Guid.Parse(hfUserID.Value));
+                        cmd.Parameters.AddWithValue("@Username", txtuserName.Text.Trim());
+                        cmd.Parameters.AddWithValue("@PasswordHash", txtPassword.Text.Trim());
+                        cmd.Parameters.AddWithValue("@LastName", txtlastName.Text.Trim());
+                        cmd.Parameters.AddWithValue("@FirstName", txtfirstName.Text.Trim());
+                        cmd.Parameters.AddWithValue("@MiddleName", txtmidName.Text.Trim());
+                        cmd.Parameters.AddWithValue("@NickName", txtnickName.Text.Trim());
+                        cmd.Parameters.AddWithValue("@EmailAddress", txtemailAdd.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Department", dlistDepartment.SelectedValue);
+                        cmd.Parameters.AddWithValue("@TimeZone", Convert.ToInt32(dlistTimeZone.SelectedValue));
+                        cmd.Parameters.AddWithValue("@UserType", Convert.ToByte(RadiouserType.SelectedValue));
+                        cmd.Parameters.AddWithValue("@UserStatus", Convert.ToByte(dlistUserstat.SelectedValue));
+
+                        //cmd.Parameters.AddWithValue("@ArchivedDate", archivedDate);
+                        //cmd.Parameters.AddWithValue("@IsIn", isIn);
+                        
+
+                        // Execute the stored procedure
+                        cmd.ExecuteNonQuery();
+                        clear();
+                        SuccessMgs.Text = "User Added Successfully!";
+                    }
                 }
             }
-            
-        }void clear()
-            {
-                hfUserID.Value = "";
-                txtuserName.Text = txtPassword.Text = txtlastName.Text = txtfirstName.Text = txtmidName.Text = txtnickName.Text = txtemailAdd.Text = "";
-                dlistDepartment.SelectedValue = "0";
-                RadiouserType.SelectedValue = "0";
-                dlistUserstat.SelectedValue = "0";
-                SuccessMgs.Text = ErrorMgs.Text = "";
-            }
+
+        }
+        void clear()
+        {
+            hfUserID.Value = "";
+            txtuserName.Text = txtPassword.Text = txtlastName.Text = txtfirstName.Text = txtmidName.Text = txtnickName.Text = txtemailAdd.Text = "";
+            dlistDepartment.SelectedValue = "0";
+            RadiouserType.SelectedValue = "0";
+            dlistUserstat.SelectedValue = "0";
+            SuccessMgs.Text = ErrorMgs.Text = "";
+        }
 
     }
 }
